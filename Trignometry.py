910312336b2d94a37f7fa92c581756a0de9e066d
AdvikGarg <gargadvik23@gmail.com>
2026-01-07 20:15:23 +0530

    Add files via upload

---
+++ Trignometry.py
@@ -0,0 +1,5 @@
+import random
+
+import math
+num=float(input('Enter a number: '))
+print(f'The sin, cos and tans value of {num} are {str(math.sin(num))}, {str(math.cos(num))}, {str(math.tan(num))} respectively')
